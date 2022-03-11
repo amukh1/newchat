44be7191c732e25dcbc322924fc2e1f5cc2a9fc4
Amukh1 <85003861+amukh1@users.noreply.github.com>
2022-03-11 14:29:19 -0500

    ihubVDJnfDNK

--- main.js
+++ main.js
@@ -2,5 +2,5 @@ function go() {
     window.localStorage.setItem('first', document.getElementById('first').value)
     window.localStorage.setItem('last', document.getElementById('last').value)
     window.localStorage.setItem('mail', document.getElementById('mail').value)
-    window.location.href = 'app.amukh1.dev/app'
+    window.location.href = 'app'
 }
